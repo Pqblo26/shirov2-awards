1b9b06cc8fddb8af5fee98e3891cb7841f875ca8
Pablo <pablo260107pmn@gmail.com>
2025-03-31 20:55:21 +0100

    Primera versión

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="text-white text-center p-10">
+      <h1 className="text-4xl font-bold mb-4">Shiro Awards 2025</h1>
+      <p>Tu código va aquí 🎉</p>
+    </main>
+  )
+}
